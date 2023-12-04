18a4f350e2a97e07718e1264ffb0143253104f71
earth <virudson@gmail.com>
2023-12-04 23:16:57 +0700

    Improve some performance

--- day02/part_two.rb
+++ day02/part_two.rb
@@ -6,11 +6,12 @@ Benchmark.bmbm do |x|
   x.report('Day 02 - Part 2') do
     sum = 0
     File.foreach('input.txt').each do |line|
+      cube_count = Hash.new(0)
       # capture any digits following color red, green, blue
-      cube_count = line.gsub(/(\d+) (red|green|blue)/).each_with_object({}) do |_m, hash|
+      line.gsub(/(\d+) (red|green|blue)/) do |_m|
         # $1: number of cubes
         # $2: color
-        hash[$2] = $1.to_i if hash[$2].to_i < $1.to_i
+        cube_count[$2] = $1.to_i if cube_count[$2] < $1.to_i
       end
 
       # The power of a set of cubes
