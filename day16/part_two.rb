d66bbecac8fb1f92eb114a2cc8343236becbb223
earth <virudson@gmail.com>
2023-12-25 15:10:32 +0700

    Day 16 part 1,2

--- day16/part_two.rb
+++ day16/part_two.rb
@@ -6,71 +6,50 @@ Benchmark.bmbm do |x|
   x.report('Day 16 - Part 2') do
     # get target coordinate by direction
     def next_position(x, y, direction)
-      case direction
-      when 'U'
-        x -= 1 if x.positive?
-      when 'D'
-        x += 1
-      when 'L'
-        y -= 1 if y.positive?
-      when 'R'
-        y += 1
-      end
-      [x, y]
+      @directions[direction].zip([x, y]).map(&:sum)
     end
 
     def move_to(x, y, direction)
-      current_tile = @fields.dig(x)&.dig(y)
-      return if current_tile.nil? # can't go to void
-      return if @travel_logs[[x, y]][direction] == 2 # passed this way
+      return if x.negative? || y.negative? || x >= @rows || y >= @cols # can't go outside
+      return if @travel_logs[[x, y]][direction].positive? # passed this way
 
       # add travel logs
       @travel_logs[[x, y]][direction] += 1
+      current_tile = @fields[x][y]
       new_direction = @rules[current_tile][direction]
 
-      case new_direction
-      when Array
+      if new_direction
         new_direction.each do
           tx, ty = next_position(x, y, _1)
           move_to(tx, ty, _1)
         end
-      when String
-        tx, ty = next_position(x, y, new_direction)
-        move_to(tx, ty, new_direction)
       else
         tx, ty = next_position(x, y, direction)
         move_to(tx, ty, direction)
       end
     end
 
-    @directions = [[-1, 0, 'D'], [0, 1, 'L'], [1, 0, 'U'], [0, -1, 'R']]
+    @directions = { 'U' => [-1, 0], 'D' => [1, 0], 'L' => [0, -1], 'R' => [0, +1] }
     @rules = {
-      '/' => { 'U' => 'R', 'D' => 'L', 'L' => 'D', 'R' => 'U' },
-      '\\' => { 'U' => 'L', 'D' => 'R', 'L' => 'U', 'R' => 'D' },
+      '/' => { 'U' => %w[R], 'D' => %w[L], 'L' => %w[D], 'R' => %w[U] },
+      '\\' => { 'U' => %w[L], 'D' => %w[R], 'L' => %w[U], 'R' => %w[D] },
       '|' => { 'L' => %w[U D], 'R' => %w[U D] },
       '-' => { 'U' => %w[L R], 'D' => %w[L R] },
       '.' => {}
     }
     @fields = File.readlines('input.txt', chomp: true).map(&:chars)
-    cols = @fields.size
-    rows = @fields[0].size
+    @rows = @fields.size
+    @cols = @fields[0].size
 
-    result = {}
-
-    # Loop through each element in the matrix
-    rows.times do |row|
-      cols.times do |col|
-        # Check for left, right, up, and down directions
-        @directions.each do |dir|
-          new_row = row + dir[0]
-          new_col = col + dir[1]
-          if new_row.negative? || new_row >= rows || new_col.negative? || new_col >= cols
-            @travel_logs = Hash.new { |h, k| h[k] = Hash.new(0) }
-            move_to(row, col, dir[2])
-            result["#{row}/#{col}:#{dir[2]}"] = @travel_logs.size
-          end
-        end
-      end
+    # left & right
+    projections = @rows.times.flat_map { [[_1, 0, 'R'], [_1, @cols - 1, 'L']] }
+    # top  bottom
+    projections += @cols.times.flat_map { [[0, _1, 'D'], [@rows - 1, _1, 'U']] }
+    result = projections.each_with_object({}) do |projection, hash|
+      @travel_logs = Hash.new { |h, k| h[k] = Hash.new(0) }
+      move_to(*projection)
+      key = "#{projection[0]}/#{projection[1]}:#{projection[2]}"
+      hash[key] = @travel_logs.size
     end
     result.values.max
   end
