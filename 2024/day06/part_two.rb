e4123afc88e2d8619cbb54ab117292b7188ebb85
earth <virudson@gmail.com>
2024-12-07 03:30:49 +0700

    2024 - Day 06 part 2 (up performance)

--- 2024/day06/part_two.rb
+++ 2024/day06/part_two.rb
@@ -1,73 +1,39 @@
 # frozen_string_literal: true
 
-start_at = nil
-@map = File.readlines('input.txt').map.with_index do |line, idx|
-  start_at = [idx, line.chars.find_index('^')] if line =~ /\^/
-  line.delete("\n").chars
-end
-
-@current_pos = start_at
-@direction_map = { r: [0, 1], b: [1, 0], l: [0, -1], t: [-1, 0] }
-@directions = %i[t r b l].reverse
-@direction = @directions.pop
-@loop_counter = 0
-
-def is_loop?
-  map = File.read('input.txt').split("\n").map(&:chars)
-  current_pos = @current_pos
-  ori_pos = @current_pos.dup
-  ori_dir = @direction.dup
-  direction = @direction.dup
-  directions = @directions.dup
-  turn_list = [{ direction => current_pos }]
-
-  # manual turn
-  directions.unshift(direction)
-  direction = directions.pop
-
-  # run until exit
+def walk(current_pos, dir, foot_steps, main_loop: false)
   loop do
-    x, y = current_pos.zip(@direction_map[direction]).map { _1.inject(:+) }
-    return false if [x, y].any?(&:negative?) || map[x].nil? || map[x][y].nil?
+    x, y = current_pos.zip(@directions.values[dir]).map(&:sum)
+    out_of_grid = [x, y].any?(&:negative?) || @map[x].nil? || @map[x][y].nil?
+    return main_loop if out_of_grid
 
-    if ori_pos == [x, y] && ori_dir == direction
-      return true
+    next_dir = (dir + 1) % 4
+    (dir = next_dir) && next if @map[x][y] == '#'
+
+    if main_loop
+      never_go = foot_steps.none? { |_facing, pos| pos == [x, y] }
+
+      # go to new way if never go
+      if never_go
+        @map[x][y] = '#'
+        @loop_counter += 1 if walk(current_pos, dir, foot_steps.clone)
+        @map[x][y] = '.'
+      end
     end
 
-    next_tile = map[x][y]
-
-    if next_tile == '#'
-      turn_list << { direction => current_pos }
-      directions.unshift(direction)
-      direction = directions.pop
-    else
-      current_pos = [x, y]
-      map[x][y] = { t: '|', r: '-', b: '|', l: '-' }[direction].green
-    end
-  end
-  true
-end
-
-loop do
-  x, y = @current_pos.zip(@direction_map[@direction]).map { _1.inject(:+) }
-  break if [x, y].any?(&:negative?) || @map[x].nil? || @map[x][y].nil?
-
-  if @map[x][y] == '#'
-    @directions.unshift(@direction)
-    @direction = @directions.pop
-  else
-    @current_pos = [x, y]
-
-    # if there are any obstacle at right side then go simulate a loop
-    next_dir = @directions.last
-    line = case next_dir
-           when :t then @map.size.times.map { |i| @map[i][y] }.join[0..x]
-           when :b then @map.size.times.map { |i| @map[i][y] }.join[x..]
-           when :r then @map[x].join[y..]
-           when :l then @map[x].join[0..y]
-           end
-    @loop_counter += 1 if line =~ /#/ && is_loop?
+    current_pos = [x, y]
+    is_added = foot_steps.add?([@facing[dir], current_pos])
+    return true if is_added.nil?
   end
 end
 
+@directions = { t: [-1, 0], r: [0, 1], b: [1, 0], l: [0, -1] }
+@facing = @directions.keys
+start_at = nil
+@map = File.read('input.txt').split("\n").map.with_index do
+  start_at = [_2, _1.chars.find_index('^')] if _1 =~ /\^/
+  _1.chars
+end
 
+foot_steps = Set.new([@facing[0], start_at])
+@loop_counter = 0
+walk(start_at, 0, foot_steps, main_loop: true)
