8ca4216d6bb337cc6987408a0a0c8fd902cea4de
earth <virudson@gmail.com>
2024-12-11 15:26:44 +0700

    2024 - Day 11 part 1

---
+++ 2024/day11/part_one.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+def blink(stone)
+  return '1' if stone == '0'
+  return (stone.to_i * 2024).to_s if stone.size.odd?
+
+  stone.chars.each_slice(stone.size / 2).map(&:join).map { _1.to_i.to_s }
+end
+
+@memory = {}
+stones = File.read('input.txt').split(' ')
+25.times do
+  stones = stones.map { blink(_1) }.flatten
+end
+stones.size
