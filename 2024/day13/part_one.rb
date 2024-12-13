09ed5d3289fcdb36ff5ae1e978d95b88e85d9250
earth <virudson@gmail.com>
2024-12-13 18:02:02 +0700

    2024 - Day 13 part 1

--- 2024/day13/part_one.rb
+++ 2024/day13/part_one.rb
@@ -12,7 +12,7 @@ chips = machines.each_slice(6).map do |(ax, ay, bx, by, px, py)|
   total_b = ((px - ax * total_a) / bx)
 
   # a bit round check seem it almost be an integer
-  next if [total_a, total_b].any? { (_1.round(5) % 1) != 0 }
+  next if [total_a, total_b].any? { (_1.round(2) % 1) != 0 }
   next if total_a > PRESS_LIMIT || total_b > PRESS_LIMIT
 
   ((3 * total_a.round) + total_b.round)
